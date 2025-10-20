de155edf0a6a1af7b4f4ab9cf17e3f5b4e94ca7d
Max <214122627+Diplomoceras-maximum@users.noreply.github.com>
2025-10-20 14:35:40 +0100

    Add explanation and examples of promises

--- script.js
+++ script.js
@@ -12,3 +12,60 @@
 // 3. Asking a user to select files using showOpenFilePicker()
 
 // Other functions may require implementing your own asynchronous functions
+
+// ##################################################
+// What is a Promise?
+// ##################################################
+
+// A Promise is a value not necessarily known when the promise is created.
+// It allows you to associate handlers with an async action's eventual success or failure.
+// This means that instead of returning the value straight away, the async method returns a promise to give the value at some point in the future.
+
+// A Promise is i one of three states:
+// 1. pending: initial state, not fulfilled or rejected (result is undefined)
+// 2. fulfilled: meaning the operation was completed successfully (result is a value)
+// 3. rejected: meaning the operation failed (result is an error object)
+
+// Example of a promise:
+let p = new Promise((resolve, reject) => {
+  let a = 1 + 1;
+  if (a == 2) {
+    resolve("Success");
+  } else {
+    reject("Failed");
+  }
+}); // define the promise
+
+p.then((message) => {
+  console.log("This is in the then: " + message);
+}).catch((message) => {
+  console.log("This is in the catch: " + message);
+}); // use the promise
+
+// then is used as the callback when the promise is successful / resolved
+// catch is used as the callback when the promise is failed / rejected
+
+// Another example:
+const recordVideoOne = new Promise((resolve, reject) => {
+  resolve("Video 1 Recorded");
+});
+
+const recordVideoTwo = new Promise((resolve, reject) => {
+  resolve("Video 2 Recorded");
+});
+
+const recordVideoThree = new Promise((resolve, reject) => {
+  resolve("Video 3 Recorded");
+});
+
+Promise.all([recordVideoOne, recordVideoTwo, recordVideoThree])
+  .then((messages) => {
+    console.log(messages);
+  })
+  .finally(() => {
+    console.log("Finished");
+  });
+
+// Promise.all is method that takes an array of promises and gives a single callback when all are resolved
+// Promise.race is another method that takes an array but instead of waiting for all promises to be resolved, it gives a callback when any promise is resolved or rejected
+// finally is a callback that is called whether the promise is a success or a failure
